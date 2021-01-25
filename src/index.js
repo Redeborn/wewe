72bc7efd061a0385563aab608963a72c0921b44b
George Negr <ivan.ivanov@gmail.com>
2021-01-25 19:52:03 +0300

    init commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const Cum = <div>
+  
+ <div className = "negr">
+   <div className = "lul"></div>
+   <div className = "negr__block"></div></div>
+ <div className = "negr"></div>
+ 
+
+</div>
+
+
+
+
+ReactDOM.render(
+  
+    <div>{Cum}</div>
+  ,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
