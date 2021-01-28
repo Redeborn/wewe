bf9429cb1a4e2fa3f3d11798056f637df511ec22
George Negr <ivan.ivanov@gmail.com>
2021-01-28 22:33:37 +0300

    intit bibit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 
 
 const Cum = <div className = "yee">
+<div className = "stolb">
+  <div className = "iii">
+    <div className = "evgen"><h1>EvgenApp</h1></div>
+    <div className = "pr"><h1><b>Предметы</b></h1></div>
+    <div className = "dz"><h1>Домашняя работа</h1></div>
+    <div className = "marks"><h1>Оценки</h1></div>
+    <div className = "dop"><h1>Доп. материал</h1></div>
+    <div className = "gei"><b>@Gladio.dev</b></div>
+  </div>
+<div className = "vv">
+
+</div>
+  </div>
   
-  <div className = "negr"></div>
-  <div className = "negr"></div>
-  <div className = "negr"></div>
-  <div className = "negr"></div>
-  
+ 
 </div>
 
 
