66b17dea815ccc6d778f545d8257ef47fbae02cc
George Negr <ivan.ivanov@gmail.com>
2021-02-04 19:20:35 +0300

    plus

--- src/index.js
+++ src/index.js
@@ -4,39 +4,11 @@ import './index.css';
 
 
 const per = <div className = "biba">
+  <button className = "plus">+</button>
   
   
-  <div className = "group">
-    <button className = "buttonact">1</button>
-    <button className  ="button">2</button>
-    <button className  ="button">3</button>
-    <button className  ="button">4</button>
-    <button className  ="button">5</button>
-  </div>
-  
-  <div className = "p">
-    <div className = "time">35:23</div>
-  </div>
-  <span className = "task">
-    <p className = "task1">Выберите True/False</p>
-    <p className = "task2">Are you gay?</p>
-
-  </span>
-  
-  
-  <div className = "kn">
-  <button className = "true">True</button>
-  <button className = "false">False</button>
-  </div>
-   
-  <div className = "useless">
-   <button className = "answer">Ответить</button>
-  </div>
-  
-  
-  
-  </div>
  
+</div>
 
 ReactDOM.render(
   
