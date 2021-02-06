fdcdb60fd4c218ba6119f9a5ffaaf479843946ce
George Negr <ivan.ivanov@gmail.com>
2021-02-06 16:09:08 +0300

    frame164

---
+++ src/frame164.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import css from "./frame164.css"
+
+
+
+const ju = <div className = "yee">
+  <div className = "card">
+      <div className = "ss">
+      <button className = "tasks">true/false</button>
+      <button className = "tasks">Выбор правильного ответа</button>
+      <button className = "tasks">Письменный ответ</button>
+      <button className = "tasks">Описание картинки</button>
+      <button className = "tasks">Описание картинки(микрофон)</button>
+      <button className = "tasks">Чтение текста(микрофон) </button>
+    </div>
+    <div className = "act">
+      <button className = "canc">Отмена</button>
+      <button className = "choose">Выбрать</button>
+
+    </div>
+  </div>
+  
+</div>
+  
+
+
+
+
+
+ReactDOM.render(
+  
+    <div>{Cum}</div>
+  ,
+  document.getElementById('root')
+);
