8de7f7a7b2b3873970d6b25c4ee68e5d21dd5d40
George Negr <ivan.ivanov@gmail.com>
2021-02-06 16:34:51 +0300

    frame 165

---
+++ src/frame165.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import css from "./frame165.css"
+
+
+
+const ju = <div className = "yee">
+  <div className = "card">
+      <div className = "ss">
+        <p className = "task">true/false</p>
+        <input placeholder="Введите вопрос" className="zapr"></input>
+    </div>
+    <div className = "act">
+      <button className = "canc">Отмена</button>
+      <button className = "choose">Выбрать</button>
+
+    </div>
+    
+  </div>
+  
+</div>
+  
+
+
