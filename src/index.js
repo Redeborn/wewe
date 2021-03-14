e5b804a9cb62c377d46786c97ad6873216f6b2c5
George Negr <ivan.ivanov@gmail.com>
2021-03-14 12:11:00 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import css from "./index.css"
 
 
-const per = <div className = "biba">
-  <button className = "plus">+</button>
-  
-  
- 
+const ju = <div className = "yee">
+  <div className = "boss">
+    <input className = "name" placeholder = "Введите имя"></input>
+    <button className = "enter">Войти в навигатор</button>
+    
+    
+  </div>
 </div>
+  
+
+
+
+
+
+
+
 
 ReactDOM.render(
   
-    <div>{per}</div>
+    <div>{ju}</div>
   ,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
